0dad3718ae6589f6cf95a5d0664ce063d0ab160d
Firas Mdimagh <firas.mdimagh@gmail.com>
2017-09-23 08:20:58 +0100

    Update sql script
    Support update of the table if it already exists

--- Workshop.DevOps/Workshop.DevOps.Db/script.sql
+++ Workshop.DevOps/Workshop.DevOps.Db/script.sql
@@ -1,10 +1,12 @@
-
-CREATE TABLE [dbo].[Item](
-	[Title] [nvarchar](50) NULL,
-	[Content] [nvarchar](max) NULL,
-	[Id] [int] IDENTITY(1,1) NOT NULL,
- CONSTRAINT [PK_Item] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)
-)
+IF OBJECT_ID('Item') IS NULL
+    EXEC(
+		'CREATE TABLE [dbo].[Item](
+			[Title] [nvarchar](50) NULL,
+			[Content] [nvarchar](max) NULL,
+			[Id] [int] IDENTITY(1,1) NOT NULL,
+		CONSTRAINT [PK_Item] PRIMARY KEY CLUSTERED 
+		(
+			[Id] ASC
+		)
+		)'
+	)
